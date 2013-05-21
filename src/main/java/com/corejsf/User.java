3a1286f7de7499007df9b385f62b7c927fc835a1
unknown <sashida@SBD751SASHIDA1.nature.local>
2013-05-21 10:10:37 +0900

    initial commit

---
+++ src/main/java/com/corejsf/User.java
@@ -0,0 +1,48 @@
+package com.corejsf;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private String password;
+	
+	public User() {
+		this("", "");
+	}
+	
+	public User(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String logout() {
+		name = "";
+		password = "";
+		
+		return "index";
+	}
+}
